a1bd430f8e0f7735afe1c74e53e93bb9bf9a3b82
ValeriySpatari <smokeyou92@gmail.com>
2023-01-29 22:03:41 +0200

    Add program on C#

---
+++ final-hw-c/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] firstArray = new string[5] { "Moldova", "Roma", "UK", "USA", "Monaco" };
+string[] finalArray = new string[firstArray.Length];
+
+void CreateArray(string[] firstArray, string[] finalArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            finalArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+CreateArray(firstArray, finalArray);
+ShowArray(finalArray);
